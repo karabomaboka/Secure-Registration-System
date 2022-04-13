e2ad64687831af45cb434644704b5df3e9a96c42
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-04-14 00:12:16 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,39 @@
+<!-- heading block -->
+  <div class="container-fluid" id="headings">
+      <h1><strong>User Registration System</strong></h1>
+      <h3 style="color: #1e1f1f;">Sample Project</h3>
+  </div>
+
+<!-- navigation bar -->
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-image: linear-gradient(-225deg, #ebf7ff 0%, #f2f2f2,  #f9fff0 100%);">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php"><b>HOMEPAGE</b></a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="display_user.php">Registered Users</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+        </li>
+      </ul>
+
+      <li class="d-flex">
+        <a href="login.php"><button class="btn btn-outline-success me-2" type="submit">LogIn</button></a>
+      </li>
+
+      <li class="d-flex">
+        <a href="signup.php"><button class="btn btn-outline-success me-2" type="submit">Register</button></a>
+      </li>
+    </div>
+  </div>
+</nav>
+
